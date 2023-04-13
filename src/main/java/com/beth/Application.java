6e5ae9ce9dfbdb3bc5348af94a4b56b7c162c924
Istvan Richter <istvan.richter@miro.technology>
2023-04-13 12:45:48 +0200

    Fix CORS config

--- src/main/java/com/beth/Application.java
+++ src/main/java/com/beth/Application.java
@@ -19,7 +19,7 @@ public class Application {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:4200", "https://beth-api.herokuapp.com");
+                        .allowedOrigins("*");
             }
         };
     }
